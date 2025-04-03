17b8eeaa2ba2059500cd88e358e62d3c0a5b0008
VeldiRahul <142107586+VeldiRahul@users.noreply.github.com>
2025-04-04 01:33:10 +0530

    Add files via upload

---
+++ DAY_2( Basic Arithmetic in C).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() 
+{
+    //Concept Of Using Operators //
+
+ /*  int num=10; 
+     float decimal=6.99;
+     char A[]="Rahul";
+     printf(" Number : %d\n",num);
+     printf("Decimal: %f\n",decimal);
+     printf("Name: %s\n",A);
+     return 0;
+      */
+
+      // Task TO Do...!//
+      int  a,b;
+      float dec,r;
+      char y='a';
+
+    printf("Please Enter two integers/ float values:  \n");
+      scanf("%d %d %f", &a,&b,&r,&dec);
+    printf("addition: %d\n",a+b);
+    printf("Subtraction : %d\n",a-b);
+    printf("multiplication: %d\n",a*b);
+    printf("Division :%f\n",a/r);
+    printf("Reminder : %d\n",a%b);
+    
+    return 0;
+
+    
+}   

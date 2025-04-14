f29840a60c7e56f8cdcb32dd27d385f1374ee14a
VeldiRahul <142107586+VeldiRahul@users.noreply.github.com>
2025-04-14 22:50:30 +0530

    Add files via upload

---
+++ Day_3/FORMATED.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+/*void main()
+{
+    char c1 ,c2[20];
+    c1=getchar();
+       putchar(c1);
+        gets(c2);
+        puts(c2);
+        getch();    }
+*/
+int	main()
+
+{
+    int a,b,y=a+b;
+    printf("enter a = ");
+    scanf("%d", &a);
+    printf("enter b = ");
+    scanf("%d", &b);
+    printf("Total = %d",(a+b));
+    return 0;
+
+}
